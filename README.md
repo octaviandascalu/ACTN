e90ea13aa3df8db3909a0c484884e6cd107b9c33
octaviandascalu <75310083+octaviandascalu@users.noreply.github.com>
2022-03-11 09:15:47 +0200

    encoding

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ACTN
  
+BAREM TEMA 1
+
+-generarea parametrului p, numar prim mare (peste 161 biti) (1p)
+  (folosind o biblioteca dedicata numerelor mari (BigInteger pentru Java, NTL/GMP pentru C/C++ etc.))
+
+
+----------------Codificare----------------
+
+-codificarea input-ului (specificat initial ca un sir de caractere) ca vector de numere naturale mai mici decat p (1p)
+-codificarea efectiva, folosind schema lui Horner pentru evaluarea polinomului P(x) in punctele 1,2,...,n (2p)
+
+
+---------------Decodificare---------------
+
+-calculul coeficientului liber - maxim (3p), dupa cum urmeaza:
+                     - varianta folosind k(k-1) inversari (1p)
+                     - varianta folosind k inversari (1p)
+                     - varianta folosind o singura inversare (1p)
+
+-comparatii din punct de vedere al timpului pentru variantele precedente (1p)
+-reconstructia polinomului P(x) in faza finala a decodificarii, creand propria functie/procedura pentru inmultirea de polinoame avand coeficienti numere naturale mai mici decat p (2p)
--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+# Implement Reed-Solomon encoding/decoding using the algorithms dis-
+# cussed in class. For simplicity, consider s = 1 (a single error may be
+# corrected). Use as few modular inversions operations as possible. (10p)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def base_10_to_p(m, p):
+    r = []
+    while m:
+        r.append(m % p)
+        m = m // p
+    # r.reverse()
+    # print(r)
+    return r
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def P(a, k, p, x):
+    val = 0
+    for i in range(k - 1, 0, -1):  # indexarea e de la 0
+        val = (val + a[i - 1]) * x
+    return val % p
+
+
+def encode(a, k, s, p):
+    y = []
+    n = k + 2 * s
+    for i in range(1, n + 1):
+        y.append(P(a, k, p, i))
+    return y
+
+
+def encoding(k, s, p, m):
+    a = base_10_to_p(m, p)
+    print(encode(a, k, s, p))
+    return 0
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    encoding(3, 1, 11, 29)
